418d3f2d93ab72598695bbb648e2f59a4e9e21ea
NissiAttakora2003 <146092409+NissiAttakora2003@users.noreply.github.com>
2025-10-20 18:02:29 +0100

    drawing shapes on the form

--- WindowsFormsAppWeek2/Form1.Designer.cs
+++ WindowsFormsAppWeek2/Form1.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnChangeDrawing = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnChangeDrawing
+            // 
+            this.btnChangeDrawing.Location = new System.Drawing.Point(345, 194);
+            this.btnChangeDrawing.Name = "btnChangeDrawing";
+            this.btnChangeDrawing.Size = new System.Drawing.Size(120, 44);
+            this.btnChangeDrawing.TabIndex = 0;
+            this.btnChangeDrawing.Text = "Change Drawing";
+            this.btnChangeDrawing.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnChangeDrawing);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnChangeDrawing;
     }
 }
 
--- WindowsFormsAppWeek2/Form1.cs
+++ WindowsFormsAppWeek2/Form1.cs
@@ -12,9 +12,51 @@ namespace WindowsFormsAppWeek2
 {
     public partial class Form1 : Form
     {
+        private bool drawShapes = true;  // keeps track of what to draw
+
         public Form1()
         {
             InitializeComponent();
+            this.Paint += new PaintEventHandler(Form1_Paint);
+
+            // Attach button click event
+            btnChangeDrawing.Click += BtnChangeDrawing_Click;
         }
+
+        private void BtnChangeDrawing_Click(object sender, EventArgs e)
+        {
+            drawShapes = !drawShapes; // flip the state
+            this.Invalidate();        // forces the form to redraw
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(Brushes.White, this.ClientRectangle);
+
+            if (drawShapes)
+            {
+                // Draw shapes
+                using (Pen bluePen = new Pen(Color.Blue, 3))
+                {
+                    e.Graphics.DrawRectangle(bluePen, 50, 50, 200, 100);
+                }
+
+                using (Pen redPen = new Pen(Color.Red, 3))
+                {
+                    e.Graphics.DrawEllipse(redPen, 300, 50, 100, 100);
+                }
+
+                e.Graphics.DrawString("Shapes Mode",
+                    new Font("Arial", 14, FontStyle.Bold), Brushes.Black, 50, 200);
+            }
+            else
+            {
+                // Draw different graphics
+                e.Graphics.FillEllipse(Brushes.Green, 100, 100, 200, 150);
+                e.Graphics.DrawString("Alternative Drawing!",
+                    new Font("Arial", 14, FontStyle.Italic), Brushes.DarkRed, 50, 300);
+            }
+        }
+
     }
 }
